751ec547a652a6f557e0e11b17b8cf553475f9ac
sadman raabith <sraabi1@lsu.edu>
2025-10-24 16:26:42 -0400

    start

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import statistics 
+import math 
+import time 
+import sys
+
+G = 1.0 
+
+#2nd order differential equation, m r ddot = sum (G mi mj / |rj - ri|**3 ) * (rj - ri)
+
